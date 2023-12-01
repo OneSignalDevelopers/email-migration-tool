6179c985eafa5438fc5af27f23020835f0ba2754
William Shepherd <williamrshepherd@gmail.com>
2023-11-30 21:52:25 -0600

    Store mailgun sending domain in cookie

--- app/common/cookies.ts
+++ app/common/cookies.ts
@@ -3,6 +3,7 @@ import { createCookie } from '@remix-run/node'
 export const prefsCookie = createCookie('prefs')
 export const onesignalAppIdKey = 'onesignal-app-id' as const
 export const mailgunApiKeyKey = 'mailgun-api-key' as const
+export const mailgunSendingDomainKey = 'mailgun-sending-domain' as const
 export const mailchimpApiKeyKey = 'mailchimp-api-key' as const
 export const mailchimpServerPrefixKey = 'mailchimp-server-prefix' as const
 export const sendgridApiKeyKey = 'sendgrid-api-key' as const
