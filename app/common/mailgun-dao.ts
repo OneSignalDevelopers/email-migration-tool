d57bbaa1bcbfc18d7e229aad33c68d7155339143
William Shepherd <williamrshepherd@gmail.com>
2023-11-30 21:52:39 -0600

    Add function to get mailing lists

---
+++ app/common/mailgun-dao.ts
@@ -0,0 +1,25 @@
+export const buildMailgunDao = (apiKey: string, sendingDomain: string) => {
+  const api = 'https://api.mailgun.net/v3'
+
+  return {
+    getMailingLists: async () => {
+      const endpoint = `${api}/lists/pages`
+
+      try {
+        const res = await fetch(endpoint, {
+          method: 'GET',
+          headers: {
+            Authorization: `Basic api:${apiKey}`,
+          },
+        })
+
+        const data = await res.json()
+
+        return { data }
+      } catch (error) {
+        console.error(error)
+        return null
+      }
+    },
+  }
+}
