fe7dd199540ecd336873e0218f0befb9e92a459a
William Shepherd <williamrshepherd@gmail.com>
2023-12-11 16:31:16 -0600

    Fix bug

--- app/common/mailgun-dao.ts
+++ app/common/mailgun-dao.ts
@@ -1,12 +1,11 @@
 export const buildMailgunDao = (apiKey: string, sendingDomain: string) => {
   const api = 'https://api.mailgun.net/v3'
+  const auth = Buffer.from(`api:${apiKey}`).toString('base64')
 
   return {
     getMailingLists: async () => {
-      const endpoint = `${api}/lists/pages`
-
       try {
-        const auth = Buffer.from(`api:${apiKey}`).toString('base64')
+        const endpoint = `${api}/lists/pages`
         const res = await fetch(endpoint, {
           method: 'GET',
           headers: {
@@ -16,6 +15,23 @@ export const buildMailgunDao = (apiKey: string, sendingDomain: string) => {
 
         const data = await res.json()
 
+        return { data }
+      } catch (error) {
+        console.error(error)
+        return null
+      }
+    },
+    getMailingListVerificationStatus: async () => {
+      try {
+        const endpoint = `${api}/domains/${sendingDomain}`
+        const res = await fetch(endpoint, {
+          method: 'GET',
+          headers: {
+            Authorization: `Basic ${auth}`,
+          },
+        })
+
+        const data = await res.json()
         return { data }
       } catch (error) {
         console.error(error)
--- app/routes/mailgun.mailing-lists.tsx
+++ app/routes/mailgun.mailing-lists.tsx
@@ -1,11 +1,11 @@
-import { ActionFunctionArgs, LoaderFunctionArgs, json } from '@remix-run/node'
+import { ActionFunctionArgs, json } from '@remix-run/node'
 import { useActionData } from '@remix-run/react'
 import { useState } from 'react'
 import {
   mailgunApiKeyKey,
+  mailgunSendingDomainKey,
   onesignalAppIdKey,
   prefsCookie,
-  sendgridApiKeyKey,
 } from '~/common/cookies'
 import { buildMailgunDao } from '~/common/mailgun-dao'
 
@@ -18,61 +18,73 @@ export default function MailgunMailingListSelection() {
     setSelection(event.currentTarget.value)
   }
 
+  const onCreateExportRecipientClicked = async () => {
+    console.log('Button Clicked')
+    const apiKey = actionData?.mailgunApiKey
+    const sendingDomain = actionData?.mailgunSendingDomain
+
+    if (!apiKey || !sendingDomain) return
+
+    const dao = buildMailgunDao(apiKey, sendingDomain)
+    const exportStatus = await dao.getMailingListVerificationStatus()
+
+    console.log(exportStatus)
+  }
+
   return (
     <>
-      <div className="max-w-lg mx-auto bg-gray-800 p-6 rounded-lg shadow-lg">
-        <div>
-          <label htmlFor="contact-list-select">
-            Choose a Mailing List to export:
-          </label>
-          <select
-            name="contact-list-select"
-            className="bg-gray-700 text-white py-2 px-4 w-full"
-            onChange={onSelectionChanged}
-          >
-            <option key="---" value="">
-              --Please choose a Mailing List--
+      <div className="mt-6">
+        <label htmlFor="contact-list-select">
+          Choose a Mailing List to export:
+        </label>
+        <select
+          name="contact-list-select"
+          className="bg-gray-700 text-white py-2 px-4 mt-2 w-full"
+          onChange={onSelectionChanged}
+        >
+          <option key="---" value="">
+            --Please choose a Mailing List--
+          </option>
+          {actionData?.mailingLists.map((item: any) => (
+            <option key={item.address} value={item.address}>
+              {item.name}
             </option>
-            {actionData?.mailingLists.map((item: any) => (
-              <option key={item.id} value={item.id}>
-                {item.name}
-              </option>
-            ))}
-          </select>
+          ))}
+        </select>
 
-          {/* {selection && (
-            <button
-              className="bg-blue-600 hover:bg-blue-700 text-white font-bold mt-4 py-2 px-4 rounded w-full"
-              onClick={onCreateExportClicked}
-            >
-              Create Export
-            </button>
-          )} */}
-
-          {/* {downloadUrl && renderDownloadLink()} */}
-        </div>
+        <button
+          className="bg-blue-600 hover:bg-blue-700 text-white font-bold mt-4 py-2 px-4 rounded w-full"
+          type="button"
+          onClick={onCreateExportRecipientClicked}
+          disabled={!selection}
+        >
+          Export Recipients
+        </button>
       </div>
     </>
   )
 }
 
 export async function action({ request }: ActionFunctionArgs) {
-  console.log('ACTION FUNCTION')
   const cookieHeader = request.headers.get('Cookie')
   const cookie = (await prefsCookie.parse(cookieHeader)) || {}
   const body = await request.formData()
   const onesignalAppId = body.get('onesignal-app-id') as string
   const mailgunApiKey = body.get('mailgun-api-key') as string
-  const sendingDomain = body.get('mailgun-sending-domain') as string
+  const mailgunSendingDomain = body.get('mailgun-sending-domain') as string
   cookie[mailgunApiKeyKey] = mailgunApiKey
+  cookie[mailgunSendingDomainKey] = mailgunSendingDomain
   cookie[onesignalAppIdKey] = onesignalAppId
-  cookie[sendgridApiKeyKey] = sendingDomain
 
-  const mailgunDao = buildMailgunDao(mailgunApiKey, sendingDomain)
+  const mailgunDao = buildMailgunDao(mailgunApiKey, mailgunSendingDomain)
   const list = await mailgunDao.getMailingLists()
 
   return json(
-    { mailingLists: list?.data || [] },
+    {
+      mailingLists: list?.data.items || [],
+      mailgunSendingDomain,
+      mailgunApiKey,
+    },
     {
       headers: {
         'Set-Cookie': await prefsCookie.serialize(cookie),
